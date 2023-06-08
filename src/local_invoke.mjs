00932c40b5b39da1b12fa3b036a95edeec1ad6af
Bogdan Abaev <bogdan@zotero.org>
2023-06-08 14:11:16 -0400

    explicitly removing events from SQS, script to poll from SQS and run lambda locally

---
+++ src/local_invoke.mjs
@@ -0,0 +1,30 @@
+/* eslint-disable indent */
+/**
+ * This file is to invoke the main function locally during tests
+ * It's needed because SAM doesn't allow to invoke a lambda that is triggered on SQS event.
+ */
+import { SQSClient, ReceiveMessageCommand } from "@aws-sdk/client-sqs";
+import config from "config";
+import { main } from "./main.mjs";
+const sqsClient = new SQSClient();
+const queueURL = config.get('sqsURL');
+
+(async () => {
+	const messages = await sqsClient.send(new ReceiveMessageCommand({ QueueUrl: queueURL, MaxNumberOfMessages: 10 }));
+	console.log(messages);
+	const event = { Records: [] };
+	if (!messages.Messages) {
+		console.log("No messages");
+		return;
+	}
+	for (let m of messages.Messages) {
+		event.Records.push({
+			messageId: m.MessageId,
+			receiptHandle: m.ReceiptHandle,
+			body: m.Body
+		});
+	}
+	await main(event);
+})();
+
+
--- src/main.mjs
+++ src/main.mjs
@@ -1,61 +1,73 @@
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { extractFulltext } from "../pdf-worker/src/index.js";
+import { SQSClient, DeleteMessageCommand } from "@aws-sdk/client-sqs";
 import fetch from 'node-fetch';
 import config from "config";
-const client = new S3Client({ region: "us-east-1" });
-
+const s3Client = new S3Client({ region: "us-east-1" });
+const sqsClient = new SQSClient();
+const queueURL = config.get('sqsURL');
 
 export const main = async (event) => {
 	// Read message from SQS
-	const body = JSON.parse(event.Records[0].body);
-	if (!body.fileName || !body.objectKey || (!body.userID && !body.groupID)) {
-		throw new Error("Required input parameter missing from event body: ", event.Records[0].body);
-	}
-	// SQS event has either userID or groupID parameter, decide which one we use
-	const urlComponent = body.userID ? "users" : "groups";
-	const urlId = body.userID || body.groupID;
-
-	var getCommandParams = {
-		Bucket: config.BUCKET,
-		Key: body.fileName
-	};
-	// Get file from S3
-	const s3Response = await client.send(new GetObjectCommand(getCommandParams));
-	const data = await s3Response.Body.transformToByteArray();
-	// Run PDF-worked
-	const extractedFullText = await extractFulltext(data);
-
-	// Hit users|groups/{id}/items/:objectKey/fulltext endpoint of dataserver with 3 retries
-	let success = false;
-	let attempts = 0;
-	let response;
-	while (!success && attempts < 3) {
-		try {
-			response = await fetch(config.DATASERVER_URL + `/${urlComponent}/${urlId}/items/${body.objectKey}/fulltext`, {
-				method: 'put',
-				body: JSON.stringify({ content: extractedFullText.text }),
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: 'Basic ' + Buffer.from(config.rootUsername + ":" + config.rootPassword).toString('base64')
-				}
-			});
-			success = true;
+	for (let record of event.Records) {
+		const body = JSON.parse(record.body);
+		if (!body.fileName || !body.itemKey || (!body.userID && !body.groupID)) {
+			throw new Error("Required input parameter missing from event body: ", event.Records[0].body);
 		}
-		catch (e) {
-			console.log(e);
-			await new Promise(r => setTimeout(r, 2000));
-			attempts += 1;
+		// SQS event has either userID or groupID parameter, decide which one we use
+		const urlComponent = body.userID ? "users" : "groups";
+		const urlId = body.userID || body.groupID;
+	
+		var getCommandParams = {
+			Bucket: config.BUCKET,
+			Key: body.fileName
+		};
+		// Get file from S3
+		const s3Response = await s3Client.send(new GetObjectCommand(getCommandParams));
+		const data = await s3Response.Body.transformToByteArray();
+		// Run PDF-worked
+		const extractedFullText = await extractFulltext(data);
+	
+		// Hit users|groups/{id}/items/:objectKey/fulltext endpoint of dataserver with 3 retries
+		let success = false;
+		let attempts = 0;
+		let response;
+		while (!success && attempts < 3) {
+			try {
+				const url = config.DATASERVER_URL + `/${urlComponent}/${urlId}/items/${body.itemKey}/fulltext`;
+				response = await fetch(url, {
+					method: 'put',
+					body: JSON.stringify({ content: extractedFullText.text }),
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: 'Basic ' + Buffer.from(config.rootUsername + ":" + config.rootPassword).toString('base64')
+					}
+				});
+				success = true;
+			}
+			catch (e) {
+				console.log(e);
+				await new Promise(r => setTimeout(r, 2000));
+				attempts += 1;
+			}
 		}
-	}
-
-	// Break if we didn't succeed
-	if (!success || response.status != 204) {
-		let message = "";
-		if (response) {
-			message += `Status: ${response.status}|`;
-			message += await response.text();
+	
+		// If we didn't succeed, skip to the next message in a batch
+		if (!success || response.status != 204) {
+			let message = "";
+			if (response) {
+				message += `Status: ${response.status}|`;
+				message += await response.text();
+			}
+			console.log(`Request to dataserver failed. MessageId: ${body.messageId}. ${message}`);
+			continue;
 		}
-		throw new Error(`Request to dataserver failed. ${message}`);
+		// If we got here, the message was processed successfully
+		// So it is deleted from the queueu
+		let deleteCommand = new DeleteMessageCommand({
+			QueueUrl: queueURL,
+			ReceiptHandle: record.receiptHandle
+		});
+		await sqsClient.send(deleteCommand);
 	}
-	return extractedFullText;
 };
